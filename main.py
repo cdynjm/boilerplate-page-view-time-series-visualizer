6ac6d3cde636abf1be0852007bc6baf0e25059ef
Jemuel Cadayona <jemuelcadayona3@gmail.com>
2025-07-18 06:45:08 +0000

    update solution

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import time_series_visualizer
-from unittest import main
+import time_series_visualizer as tsv
 
-# Test your function by calling it here
-time_series_visualizer.draw_line_plot()
-time_series_visualizer.draw_bar_plot()
-time_series_visualizer.draw_box_plot()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# Generate plots
+tsv.draw_line_plot()
+tsv.draw_bar_plot()
+tsv.draw_box_plot()
